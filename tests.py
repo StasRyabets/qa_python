452edf90d85ad5741a0e7fc3682c80a3a116d56e
Stas Ryabets <stas.ryabets@mail.ru>
2023-09-11 16:13:08 +0500

    add assert message in test_add_new_book_add_two_books

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class TestBooksCollector:
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-        assert len(collector.get_books_genre()) == 2
+        assert len(collector.get_books_genre()) == 2, "get_books_genre вернул НЕ две книги"
 
     def test_add_new_book_add_duplicate_book(self):
         collector = BooksCollector()
